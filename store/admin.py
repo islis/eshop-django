b29260baa814bd14fbbf21b79aa4fecf95818c52
islis <soulmastre2@gmail.com>
2021-05-25 22:07:24 +0500

    Это мой первый commit

---
+++ store/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('product_name',)}
+    list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available')
+    list_filter =('category',)
+
+# admin.site.register(Product, ProductAdmin)
