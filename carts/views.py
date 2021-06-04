528cc6b3cecd09c27d4efa936904e8a129127ea9
islis <soulmastre2@gmail.com>
2021-06-05 00:00:18 +0500

    исправлена ошибка корзины, добавлен счётчик товаров в корзине, добавлен пагинатор

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -54,18 +55,18 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
 
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
-    tax = (2 * total) / 100
-    grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass # just ignore
 
 
     context = {
