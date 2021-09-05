a2eda6fc5e81c5c4147f437d7982cb01eee831e2
islis <soulmastre2@gmail.com>
2021-09-05 20:06:34 +0500

    Добавлена вариация продукта

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 # Register your models here.
 
 @admin.register(Product)
@@ -8,4 +8,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available')
     list_filter =('category',)
 
-# admin.site.register(Product, ProductAdmin)
+@admin.register(Variation)
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
+
+# admin.site.register(Variation)
